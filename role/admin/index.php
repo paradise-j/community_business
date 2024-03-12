6914a086052558ce43956c998407b2865a1ed6de
paradise-j <warastudio61@gmail.com>
2024-03-12 23:50:56 +0700

    Update index.php

--- role/admin/index.php
+++ role/admin/index.php
@@ -10,7 +10,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -28,7 +27,6 @@
 
         <!-- Custom styles for this template-->
         <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
     </head>
 
     <body id="page-top">
@@ -214,6 +212,5 @@
         <script src="js/demo/chart-area-demo.js"></script>
         <script src="js/demo/chart-pie-demo.js"></script>
         <script src="js/demo/chart-bar-demo.js"></script>
-
     </body>
 </html>
