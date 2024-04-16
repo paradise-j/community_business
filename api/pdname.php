8dca2025f682578129331a3f8d4b3d8f442d3386
paradise-j <warastudio61@gmail.com>
2024-04-16 20:04:46 +0700

    Update pdname.php

--- api/pdname.php
+++ api/pdname.php
@@ -1,17 +1,14 @@
 <?php
     require_once '../connect.php';
     session_start();
-    echo "1";
-    // if(isset($_POST['function']) and $_POST['function'] == 'pname'){
-        echo "1";
+    if(isset($_POST['function']) and $_POST['function'] == 'pdname'){
         $id = $_POST['id'];
-        // $id = 4;
         $stmt = $db->query("SELECT `pd_avgprice` as price FROM `product` WHERE `pd_id` = '$id'");
         $stmt->execute();
 
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         echo $row["price"];
         exit();
-    // }
+    }
     
 ?>
