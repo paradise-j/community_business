c8350aa4a77aebf0150de8225e444b97f62e8650
paradise-j <warastudio61@gmail.com>
2024-06-27 22:42:41 +0700

    Update index.php

--- role/admin/index.php
+++ role/admin/index.php
@@ -211,6 +211,6 @@
         <!-- Page level custom scripts -->
         <script src="js/demo/chart-area-demo.js"></script>
         <script src="js/demo/chart-pie-demo.js"></script>
-        <script src="js/demo/chart-bar-demo.js"></script>
+        <!-- <script src="js/demo/chart-bar-demo.js"></script> -->
     </body>
 </html>
