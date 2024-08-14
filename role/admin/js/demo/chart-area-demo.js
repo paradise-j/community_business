370a33e5677f3f18c08077887ccdc8493e9b257a
paradise-j <warastudio61@gmail.com>
2024-08-14 23:11:50 +0700

    Update chart-area-demo.js

--- role/admin/js/demo/chart-area-demo.js
+++ role/admin/js/demo/chart-area-demo.js
@@ -140,7 +140,7 @@ window.addEventListener('DOMContentLoaded', async (event) => {
         labels: Unique_label,
         datasets: [{
           label: "รายได้แฝง",
-          lineTension: 0.3,
+          lineTension: 0,
           backgroundColor: "rgba(78, 115, 223, 0.07)",
           borderColor: "rgba(78, 115, 223, 1)",
           pointRadius: 5,
